a1d77f6746adbbc4f6da601b5152693618976b10
NMI <nmi@equifax.ru>
2022-06-15 17:17:04 +0300

    tk

--- controllers/HookController.php
+++ controllers/HookController.php
@@ -6,7 +6,7 @@ class HookController extends \yii\rest\Controller
 {
     public function actionIndex()
     {
-        file_put_contents(__DIR__ . '../runtime/hook.log', $this->request, FILE_APPEND);
+        file_put_contents(__DIR__ . DIRECTORY_SEPARATOR . '../runtime/hook.log', $this->request, FILE_APPEND);
         return $this->response;
     }
 }
