6b4c68280f0013733b2363ac4133279572f6b946
NMI <nmi@equifax.ru>
2022-06-15 17:09:14 +0300

    tk

--- config/params.php
+++ config/params.php
@@ -6,5 +6,6 @@ return [
     'adminEmail' => 'admin@example.com',
     'senderEmail' => 'noreply@example.com',
     'senderName' => 'Example.com mailer',
-    'tg_token' => $config['tg_token'] ?? ''
+    'tg_token' => $config['tg_token'] ?? '',
+    'hook_url' => $config['hook_url'] ?? ''
 ];
