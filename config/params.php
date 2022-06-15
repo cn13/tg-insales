bc4a692056c48884116c6e1a72aa75a767893bcb
Mike N <m@cn13.ru>
2022-06-15 21:54:26 +0300

    hp

--- config/params.php
+++ config/params.php
@@ -1,6 +1,6 @@
 <?php
 
-$config = json_decode(file_get_contents('config/config.json'), true);
+$config = json_decode(file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . 'config.json'), true);
 
 return [
     'adminEmail'  => 'admin@example.com',
