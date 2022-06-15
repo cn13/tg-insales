28d7e6bfd64ee70e38cc56c5cd409740e778d8c0
NMI <nmi@equifax.ru>
2022-06-15 13:35:25 +0300

    init

--- commands/InstallController.php
+++ commands/InstallController.php
@@ -6,6 +6,6 @@ class InstallController extends \yii\console\Controller
 {
     public function actionIndex()
     {
-        file_put_contents('../runtime/requiest_install.log', print_r($_REQUEST ?? [], 1) . PHP_EOL, FILE_APPEND);
+        file_put_contents('runtime/requiest_install.log', print_r($_REQUEST ?? [], 1) . PHP_EOL, FILE_APPEND);
     }
 }
