09eaf015c64848acb78b58b19d4df6c39b8f5221
NMI <nmi@equifax.ru>
2022-06-16 10:51:31 +0300

    install

--- controllers/AppController.php
+++ controllers/AppController.php
@@ -6,11 +6,16 @@ class AppController extends \yii\web\Controller
 {
     public function actionInstall()
     {
-        file_put_contents('runtime/install.log', print_r($_REQUEST ?? [], 1) . PHP_EOL, FILE_APPEND);
+        file_put_contents(__DIR__ . '/../runtime/install.log', print_r($_REQUEST ?? [], 1) . PHP_EOL, FILE_APPEND);
     }
 
     public function actionUninstall()
     {
-        file_put_contents('runtime/install.log', print_r($_REQUEST ?? [], 1) . PHP_EOL, FILE_APPEND);
+        file_put_contents(__DIR__ . '/../runtime/uninstall.log', print_r($_REQUEST ?? [], 1) . PHP_EOL, FILE_APPEND);
+    }
+
+    public function actionLogin()
+    {
+        file_put_contents(__DIR__ . '/../runtime/login.log', print_r($_REQUEST ?? [], 1) . PHP_EOL, FILE_APPEND);
     }
 }
