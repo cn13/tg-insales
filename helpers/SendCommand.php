e282abf11c38dea9bded4e880e4f78442b5c224e
NMI <nmi@equifax.ru>
2022-06-17 10:50:17 +0300

    install

--- helpers/SendCommand.php
+++ helpers/SendCommand.php
@@ -30,10 +30,10 @@ class SendCommand
      */
     private function curl($url, $data)
     {
-        //$data['text'] = '<pre>' . $data['text'] . '</pre>';
-        foreach ($data as $k => $v) {
+        $data['text'] = '<pre>' . $data['text'] . '</pre>';
+        /*foreach ($data as $k => $v) {
             $data[$k] = urlencode($v);
-        }
+        }*/
 
         $ch = curl_init();
         //  set the url
