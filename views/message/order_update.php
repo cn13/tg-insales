9387ad38693f68a4026341bd9460a35909c90579
NMI <nmi@equifax.ru>
2022-06-17 10:56:06 +0300

    install

--- views/message/order_update.php
+++ views/message/order_update.php
@@ -5,5 +5,6 @@ $change = $order['order_changes'][0];
 ?>
 Изменение в заказе <?= $order['number'] ?>:
     <?= date('d.m.Y H:i', strtotime($change['created_at'])) ?> <?= $change['full_description'] ?>
+
 --
 Сумма: <?= $order['total_price'] ?>
