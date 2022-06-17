e326dd7469e4ce0f924680904f4f2d5afad55f35
NMI <nmi@equifax.ru>
2022-06-17 10:28:13 +0300

    install

--- helpers/SendCommand.php
+++ helpers/SendCommand.php
@@ -17,10 +17,27 @@ class SendCommand
     public function send(string $cmd, array $params = []): array
     {
         $url = "https://api.telegram.org/bot{$this->token}/$cmd";
-        if (!empty($params)) {
+        /*if (!empty($params)) {
             $url .= '?' . http_build_query($params);
-        }
-        return json_decode(file_get_contents($url), true);
+        }*/
+        return json_decode($this->curl($url, $params), true);
+    }
+
+    /**
+     * @param $url
+     * @param $params
+     * @return bool|string
+     */
+    private function curl($url, $params)
+    {
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($params));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+        $result = curl_exec($ch);
+        curl_close($ch);
+        return $result;
     }
 
     /**
