6d12c7bf50455e52cf5c773447c4a5a77fa9410b
Mike N <m@cn13.ru>
2022-11-29 22:16:59 +0300

    INIT

--- config/db.php
+++ config/db.php
@@ -3,17 +3,11 @@
 $config = json_decode(file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . 'config.json'), true);
 
 return [
-    'class' => 'yii\db\Connection',
-    'dsn' => sprintf('pgsql:host=%s;dbname=%s', $config['db']['host'], $config['db']['dbname']),
+    'class'    => 'yii\db\Connection',
+    'dsn'      => sprintf('mysql:host=%s;dbname=%s', $config['db']['host'], $config['db']['dbname']),
     'username' => $config['db']['username'],
     'password' => $config['db']['password'],
-    'charset' => 'utf8',
-    'schemaMap' => [
-        'pgsql' => [
-            'class' => 'yii\db\pgsql\Schema',
-            'defaultSchema' => $config['db']['schema'] ?? 'public'
-        ]
-    ],
+    'charset'  => 'utf8'
 ];
 
 
