eb6e4eccc90b39be61c13c8f0d530d4667045924
NMI <nmi@equifax.ru>
2022-12-06 14:27:56 +0300

    add check to TG

--- helpers/SlashCommand.php
+++ helpers/SlashCommand.php
@@ -71,7 +71,11 @@ class SlashCommand
 
         $r = [
             "photo" => $card->getQrLink(),
-            "caption" => 'Ваша карта лояльности!' . PHP_EOL . 'Скидка ' . $card->value . '%'
+            "caption" => str_pad(
+                    '',
+                    random_int(1, 12),
+                    '-'
+                ) . PHP_EOL . 'Ваша карта лояльности!' . PHP_EOL . ' Скидка ' . $card->value . '%'
         ];
 
         file_put_contents('../runtime/result.log', print_r($r, 1), FILE_APPEND);
