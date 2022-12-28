da83db8963fd694cf1ad16f164daa9e0527db1f6
Mike N <m@cn13.ru>
2022-12-28 10:52:04 +0300

    INIT

--- views/site/index.php
+++ views/site/index.php
@@ -1,1 +1,15 @@
-SmokeLife.RU
+<div id="root">
+    <div class="bc bc-tiles bc-withjinologo">
+        <div class="bc-head bc-head-nologo">
+            <div class="bc-texts">
+                <h1 class="bc-title">SmokeLife.RU</h1>
+                <p class="bc-text">Электронные сигареты, ПОДы, железки, жижи.</p>
+            </div>
+        </div>
+        <ul class="bc-links bc-links-3">
+            <li class="bc-link bc-link-phone bc-link-defaultcolor"><span class="bc-link-icon"></span><a href="tel:+79162807248" class="bc-link-anchor">+7 916 2807248</a></li>
+            <li class="bc-link bc-link-email bc-link-defaultcolor"><span class="bc-link-icon"></span><a href="mailto:m@cn13.ru" class="bc-link-anchor">m@cn13.ru</a></li>
+            <li class="bc-link bc-link-telegram bc-link-defaultcolor"><span class="bc-link-icon"></span><a href="https://telegram.me/smokelife_ru" class="bc-link-anchor" target="_blank" rel="nofollow">smokelife_ru</a></li>
+        </ul>
+    </div>
+</div>
